f2c38b45a0c407fc39c7485ddea3589a08cde615
clepix21 <clement.lemaire2107@gmail.com>
2024-05-28 17:57:08 +0200

    bientot fini

--- main/Form1.Designer.cs
+++ main/Form1.Designer.cs
@@ -29,6 +29,11 @@
             label2 = new Label();
             textBoxCIDR = new TextBox();
             label3 = new Label();
+            textBoxSubnetMask = new TextBox();
+            label4 = new Label();
+            buttonClear = new Button();
+            label5 = new Label();
+            labelMachinesCount = new Label();
             SuspendLayout();
             // 
             // label1
@@ -45,6 +50,7 @@
             // 
             textBoxDecimalIPAddress.Location = new Point(261, 20);
             textBoxDecimalIPAddress.Margin = new Padding(4, 5, 4, 5);
+            textBoxDecimalIPAddress.MaxLength = 15;
             textBoxDecimalIPAddress.Name = "textBoxDecimalIPAddress";
             textBoxDecimalIPAddress.Size = new Size(179, 27);
             textBoxDecimalIPAddress.TabIndex = 1;
@@ -54,6 +60,7 @@
             // 
             textBoxBinaryIPAddress.Location = new Point(261, 60);
             textBoxBinaryIPAddress.Margin = new Padding(4, 5, 4, 5);
+            textBoxBinaryIPAddress.MaxLength = 35;
             textBoxBinaryIPAddress.Name = "textBoxBinaryIPAddress";
             textBoxBinaryIPAddress.Size = new Size(179, 27);
             textBoxBinaryIPAddress.TabIndex = 2;
@@ -73,72 +80,79 @@
             // labelClass
             // 
             labelClass.AutoSize = true;
-            labelClass.Location = new Point(16, 140);
+            labelClass.Location = new Point(16, 235);
             labelClass.Margin = new Padding(4, 0, 4, 0);
             labelClass.Name = "labelClass";
             labelClass.Size = new Size(151, 20);
             labelClass.TabIndex = 4;
             labelClass.Text = "Classe de l'adresse IP:";
+            labelClass.Visible = false;
             // 
             // labelSubnetMask
             // 
             labelSubnetMask.AutoSize = true;
-            labelSubnetMask.Location = new Point(16, 180);
+            labelSubnetMask.Location = new Point(16, 295);
             labelSubnetMask.Margin = new Padding(4, 0, 4, 0);
             labelSubnetMask.Name = "labelSubnetMask";
-            labelSubnetMask.Size = new Size(204, 20);
+            labelSubnetMask.Size = new Size(171, 20);
             labelSubnetMask.TabIndex = 5;
-            labelSubnetMask.Text = "Masque de sous-réseau CIDR:";
+            labelSubnetMask.Text = "Masque de sous-réseau :";
+            labelSubnetMask.Visible = false;
             // 
             // labelNetworkAddress
             // 
             labelNetworkAddress.AutoSize = true;
-            labelNetworkAddress.Location = new Point(16, 220);
+            labelNetworkAddress.Location = new Point(16, 265);
             labelNetworkAddress.Margin = new Padding(4, 0, 4, 0);
             labelNetworkAddress.Name = "labelNetworkAddress";
             labelNetworkAddress.Size = new Size(132, 20);
             labelNetworkAddress.TabIndex = 6;
             labelNetworkAddress.Text = "Adresse de réseau:";
+            labelNetworkAddress.Visible = false;
             // 
             // labelBroadcastAddress
             // 
             labelBroadcastAddress.AutoSize = true;
-            labelBroadcastAddress.Location = new Point(16, 260);
+            labelBroadcastAddress.Location = new Point(16, 325);
             labelBroadcastAddress.Margin = new Padding(4, 0, 4, 0);
             labelBroadcastAddress.Name = "labelBroadcastAddress";
             labelBroadcastAddress.Size = new Size(155, 20);
             labelBroadcastAddress.TabIndex = 7;
             labelBroadcastAddress.Text = "Adresse de broadcast:";
+            labelBroadcastAddress.Visible = false;
             // 
             // labelFirstIPAddress
             // 
             labelFirstIPAddress.AutoSize = true;
-            labelFirstIPAddress.Location = new Point(16, 300);
+            labelFirstIPAddress.Location = new Point(16, 355);
             labelFirstIPAddress.Margin = new Padding(4, 0, 4, 0);
             labelFirstIPAddress.Name = "labelFirstIPAddress";
             labelFirstIPAddress.Size = new Size(141, 20);
             labelFirstIPAddress.TabIndex = 8;
             labelFirstIPAddress.Text = "Première adresse IP:";
+            labelFirstIPAddress.Visible = false;
             // 
             // labelLastIPAddress
             // 
             labelLastIPAddress.AutoSize = true;
-            labelLastIPAddress.Location = new Point(16, 340);
+            labelLastIPAddress.Location = new Point(16, 385);
             labelLastIPAddress.Margin = new Padding(4, 0, 4, 0);
             labelLastIPAddress.Name = "labelLastIPAddress";
             labelLastIPAddress.Size = new Size(139, 20);
             labelLastIPAddress.TabIndex = 9;
             labelLastIPAddress.Text = "Dernière adresse IP:";
+            labelLastIPAddress.Visible = false;
             // 
             // labelIPCount
             // 
             labelIPCount.AutoSize = true;
-            labelIPCount.Location = new Point(16, 380);
+            labelIPCount.Location = new Point(16, 415);
             labelIPCount.Margin = new Padding(4, 0, 4, 0);
             labelIPCount.Name = "labelIPCount";
             labelIPCount.Size = new Size(235, 20);
             labelIPCount.TabIndex = 10;
             labelIPCount.Text = "Nombre d'adresses IP disponibles:";
+            labelIPCount.Visible = false;
             // 
             // label2
             // 
@@ -154,6 +168,7 @@
             // 
             textBoxCIDR.Location = new Point(261, 100);
             textBoxCIDR.Margin = new Padding(4, 5, 4, 5);
+            textBoxCIDR.MaxLength = 2;
             textBoxCIDR.Name = "textBoxCIDR";
             textBoxCIDR.Size = new Size(179, 27);
             textBoxCIDR.TabIndex = 12;
@@ -169,22 +184,67 @@
             label3.TabIndex = 13;
             label3.Text = "Entrez le CIDR:";
             // 
-            // BoxSubnetMask
+            // textBoxSubnetMask
             // 
-            textBoxSubnetMask = new TextBox();
             textBoxSubnetMask.Location = new Point(261, 140);
             textBoxSubnetMask.Margin = new Padding(4, 5, 4, 5);
+            textBoxSubnetMask.MaxLength = 15;
             textBoxSubnetMask.Name = "textBoxSubnetMask";
             textBoxSubnetMask.Size = new Size(179, 27);
-            textBoxSubnetMask.TabIndex = 14; // Ajustez le numéro de tabulation si nécessaire
-            Controls.Add(textBoxSubnetMask);
+            textBoxSubnetMask.TabIndex = 14;
             textBoxSubnetMask.TextChanged += textBoxSubnetMask_TextChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(16, 140);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(193, 20);
+            label4.TabIndex = 15;
+            label4.Text = "Entrez le masque de réseau:";
+            // 
+            // buttonClear
+            // 
+            buttonClear.Location = new Point(448, 100);
+            buttonClear.Margin = new Padding(4, 5, 4, 5);
+            buttonClear.Name = "buttonClear";
+            buttonClear.Size = new Size(100, 75);
+            buttonClear.TabIndex = 16;
+            buttonClear.Text = "Effacer";
+            buttonClear.UseVisualStyleBackColor = true;
+            buttonClear.Click += buttonClear_Click;
+            // 
+            // label5
+            // 
+            label5.BorderStyle = BorderStyle.Fixed3D;
+            label5.Location = new Point(11, 218);
+            label5.Name = "label5";
+            label5.Size = new Size(540, 2);
+            label5.TabIndex = 18;
+            label5.Text = "label5";
+            // 
+            // labelMachinesCount
+            // 
+            labelMachinesCount.AutoSize = true;
+            labelMachinesCount.Location = new Point(16, 445);
+            labelMachinesCount.Margin = new Padding(4, 0, 4, 0);
+            labelMachinesCount.Name = "labelMachinesCount";
+            labelMachinesCount.Size = new Size(234, 20);
+            labelMachinesCount.TabIndex = 19;
+            labelMachinesCount.Text = "Nombre de machines disponibles:";
+            labelMachinesCount.Visible = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(563, 411);
+            ClientSize = new Size(563, 502);
+            Controls.Add(labelMachinesCount);
+            Controls.Add(label5);
+            Controls.Add(buttonClear);
+            Controls.Add(label4);
+            Controls.Add(textBoxSubnetMask);
             Controls.Add(label3);
             Controls.Add(textBoxCIDR);
             Controls.Add(label2);
@@ -221,6 +281,10 @@
         private System.Windows.Forms.TextBox textBoxCIDR;
         private Label label3;
         private System.Windows.Forms.TextBox textBoxSubnetMask;
-
+        private Label label4;
+        private System.Windows.Forms.Button buttonClear;
+        private Label label5;
+        private Label label6;
+        private Label labelMachinesCount;
     }
 }
--- main/Form1.cs
+++ main/Form1.cs
@@ -39,67 +39,146 @@ namespace IPAddressCalculator
             string inputCIDR = textBoxCIDR.Text;
             string inputSubnetMask = textBoxSubnetMask.Text;
 
-            string input = !string.IsNullOrEmpty(inputDecimal) ? inputDecimal : inputBinary;
-            int cidr;
+            // Reset labels
+            ResetLabels();
 
-            if (!string.IsNullOrEmpty(inputCIDR))
+            // Determine IP address input type
+            string ipString = DetermineIPAddress(inputDecimal, inputBinary);
+            if (ipString == null)
             {
-                if (!int.TryParse(inputCIDR, out cidr) || cidr < 0 || cidr > 32)
-                {
-                    MessageBox.Show("CIDR invalide. Il doit être entre 0 et 32.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-            }
-            else if (!string.IsNullOrEmpty(inputSubnetMask))
-            {
-                cidr = SubnetMaskToCIDR(inputSubnetMask);
-                if (cidr == -1)
-                {
-                    MessageBox.Show("Masque de sous-réseau invalide.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-            }
-            else
-            {
-                MessageBox.Show("Veuillez entrer un CIDR ou un masque de sous-réseau.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Adresse IP invalide.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
-            string ipString = input;
-            if (!string.IsNullOrEmpty(inputBinary) && IsBinaryFormat(ipString))
+            int cidr = DetermineCIDR(inputCIDR, inputSubnetMask);
+            if (cidr == -1)
             {
-                ipString = ConvertBinaryToDecimal(ipString);
+                return;
             }
 
             if (IPAddress.TryParse(ipString, out IPAddress address))
             {
-                string ipClass = GetIPClass(address);
-                IPAddress subnetMask = GetSubnetMaskFromCIDR(cidr);
-                IPAddress networkAddress = GetNetworkAddress(address, subnetMask);
-                IPAddress broadcastAddress = GetBroadcastAddress(networkAddress, subnetMask);
-                IPAddress firstIPAddress = GetFirstIPAddress(networkAddress);
-                IPAddress lastIPAddress = GetLastIPAddress(broadcastAddress);
-                int availableIPCount = GetAvailableIPCount(subnetMask);
+                if (IsReservedIPAddress(address))
+                {
+                    MessageBox.Show("Les adresses IP dans les plages de 0.0.0.0 à 0.255.255.255 et de 127.0.0.0 à 127.255.255.255 ne sont pas autorisées.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
 
-                labelClass.Text = $"Classe de l'adresse IP: {ipClass}";
-                labelSubnetMask.Text = $"Masque de sous-réseau CIDR: {subnetMask}";
-                labelNetworkAddress.Text = $"Adresse de réseau: {networkAddress}";
-                labelBroadcastAddress.Text = $"Adresse de broadcast: {broadcastAddress}";
-                labelFirstIPAddress.Text = $"Première adresse IP: {firstIPAddress}";
-                labelLastIPAddress.Text = $"Dernière adresse IP: {lastIPAddress}";
-                labelIPCount.Text = $"Nombre d'adresses IP disponibles: {availableIPCount}";
+                DisplayCalculatedValues(address, cidr);
             }
             else
             {
                 MessageBox.Show("Adresse IP invalide.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+
+            labelClass.Visible = true;
+            labelSubnetMask.Visible = true;
+            labelNetworkAddress.Visible = true;
+            labelBroadcastAddress.Visible = true;
+            labelFirstIPAddress.Visible = true;
+            labelLastIPAddress.Visible = true;
+            labelIPCount.Visible = true;
+            labelMachinesCount.Visible = true;
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            // Effacer le texte des champs de texte
+            textBoxDecimalIPAddress.Text = string.Empty;
+            textBoxBinaryIPAddress.Text = string.Empty;
+            textBoxCIDR.Text = string.Empty;
+            textBoxSubnetMask.Text = string.Empty;
+
+            // Cacher les labels des résultats
+            labelClass.Visible = false;
+            labelSubnetMask.Visible = false;
+            labelNetworkAddress.Visible = false;
+            labelBroadcastAddress.Visible = false;
+            labelFirstIPAddress.Visible = false;
+            labelLastIPAddress.Visible = false;
+            labelIPCount.Visible = false;
+            labelMachinesCount.Visible = false;
+
+        }
+        private void ResetLabels()
+        {
+            labelClass.Text = string.Empty;
+            labelSubnetMask.Text = string.Empty;
+            labelNetworkAddress.Text = string.Empty;
+            labelBroadcastAddress.Text = string.Empty;
+            labelFirstIPAddress.Text = string.Empty;
+            labelLastIPAddress.Text = string.Empty;
+            labelIPCount.Text = string.Empty;
+            labelMachinesCount.Text = string.Empty;
+        }
+
+        private string DetermineIPAddress(string inputDecimal, string inputBinary)
+        {
+            if (!string.IsNullOrEmpty(inputBinary) && IsBinaryFormat(inputBinary))
+            {
+                return ConvertBinaryToDecimal(inputBinary);
+            }
+            return inputDecimal;
+        }
+
+        private int DetermineCIDR(string inputCIDR, string inputSubnetMask)
+        {
+            if (!string.IsNullOrEmpty(inputCIDR))
+            {
+                if (int.TryParse(inputCIDR, out int cidr) && cidr >= 0 && cidr <= 32)
+                {
+                    return cidr;
+                }
+                MessageBox.Show("CIDR invalide. Il doit être entre 0 et 32.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (!string.IsNullOrEmpty(inputSubnetMask))
+            {
+                int cidr = SubnetMaskToCIDR(inputSubnetMask);
+                if (cidr != -1)
+                {
+                    return cidr;
+                }
+                MessageBox.Show("Masque de sous-réseau invalide.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Veuillez entrer un CIDR ou un masque de sous-réseau.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return -1;
+        }
+
+        private void DisplayCalculatedValues(IPAddress address, int cidr)
+        {
+            string ipClass = GetIPClass(address);
+            IPAddress subnetMask = GetSubnetMaskFromCIDR(cidr);
+            IPAddress networkAddress = GetNetworkAddress(address, subnetMask);
+            IPAddress broadcastAddress = GetBroadcastAddress(networkAddress, subnetMask);
+            IPAddress firstIPAddress = GetFirstIPAddress(networkAddress);
+            IPAddress lastIPAddress = GetLastIPAddress(broadcastAddress);
+            int availableIPCount = GetAvailableIPCount(subnetMask);
+            int availableMachinesCount = GetAvailableMachinesCount(subnetMask);
+
+            labelClass.Text = $"Classe de l'adresse IP : {ipClass}";
+            labelSubnetMask.Text = $"Masque de sous-réseau CIDR : {subnetMask}";
+            labelNetworkAddress.Text = $"Adresse de réseau : {networkAddress}";
+            labelBroadcastAddress.Text = $"Adresse de broadcast : {broadcastAddress}";
+            labelFirstIPAddress.Text = $"Première adresse IP : {firstIPAddress}";
+            labelLastIPAddress.Text = $"Dernière adresse IP : {lastIPAddress}";
+            labelIPCount.Text = $"Nombre d'adresses IP disponibles : {availableIPCount}";
+            labelMachinesCount.Text = $"Nombre de machines disponibles : {availableMachinesCount}";
+        }
+
+        private bool IsReservedIPAddress(IPAddress ipAddress)
+        {
+            byte[] addressBytes = ipAddress.GetAddressBytes();
+            return (addressBytes[0] == 0) || (addressBytes[0] == 127);
         }
 
         private bool IsBinaryFormat(string ipString)
         {
             string[] parts = ipString.Split('.');
             if (parts.Length != 4) return false;
-            return parts.All(part => part.All(c => c == '0' || c == '1'));
+            return parts.All(part => part.Length == 8 && part.All(c => c == '0' || c == '1'));
         }
 
         private string ConvertBinaryToDecimal(string binaryIP)
@@ -117,56 +196,33 @@ namespace IPAddressCalculator
 
         private int SubnetMaskToCIDR(string subnetMask)
         {
-            IPAddress mask;
-            if (IPAddress.TryParse(subnetMask, out mask))
+            if (IPAddress.TryParse(subnetMask, out IPAddress mask))
             {
                 byte[] bytes = mask.GetAddressBytes();
-                uint maskValue = BitConverter.ToUInt32(bytes.Reverse().ToArray(), 0);
-                int cidr = 0;
-                while (maskValue != 0)
-                {
-                    cidr += (int)(maskValue & 1);
-                    maskValue >>= 1;
-                }
+                int cidr = bytes.Sum(b => Convert.ToString(b, 2).Count(c => c == '1'));
                 return cidr;
             }
             return -1;
         }
 
-        static string GetIPClass(IPAddress ipAddress)
+        private string GetIPClass(IPAddress ipAddress)
         {
             byte firstOctet = ipAddress.GetAddressBytes()[0];
-            if (firstOctet >= 1 && firstOctet <= 126)
-            {
-                return "A";
-            }
-            else if (firstOctet >= 128 && firstOctet <= 191)
-            {
-                return "B";
-            }
-            else if (firstOctet >= 192 && firstOctet <= 223)
-            {
-                return "C";
-            }
-            else if (firstOctet >= 224 && firstOctet <= 239)
-            {
-                return "D";
-            }
-            else if (firstOctet >= 240 && firstOctet <= 255)
-            {
-                return "E";
-            }
+            if (firstOctet >= 1 && firstOctet <= 126) return "A";
+            if (firstOctet >= 128 && firstOctet <= 191) return "B";
+            if (firstOctet >= 192 && firstOctet <= 223) return "C";
+            if (firstOctet >= 224 && firstOctet <= 239) return "D";
+            if (firstOctet >= 240 && firstOctet <= 255) return "E";
             return null;
         }
 
-        static IPAddress GetSubnetMaskFromCIDR(int cidr)
+        private IPAddress GetSubnetMaskFromCIDR(int cidr)
         {
-            uint mask = 0xffffffff;
-            mask <<= (32 - cidr);
+            uint mask = 0xffffffff << (32 - cidr);
             return new IPAddress(BitConverter.GetBytes(mask).Reverse().ToArray());
         }
 
-        static IPAddress GetNetworkAddress(IPAddress ipAddress, IPAddress subnetMask)
+        private IPAddress GetNetworkAddress(IPAddress ipAddress, IPAddress subnetMask)
         {
             byte[] ipBytes = ipAddress.GetAddressBytes();
             byte[] maskBytes = subnetMask.GetAddressBytes();
@@ -179,7 +235,7 @@ namespace IPAddressCalculator
             return new IPAddress(networkBytes);
         }
 
-        static IPAddress GetBroadcastAddress(IPAddress networkAddress, IPAddress subnetMask)
+        private IPAddress GetBroadcastAddress(IPAddress networkAddress, IPAddress subnetMask)
         {
             byte[] networkBytes = networkAddress.GetAddressBytes();
             byte[] maskBytes = subnetMask.GetAddressBytes();
@@ -187,30 +243,35 @@ namespace IPAddressCalculator
 
             for (int i = 0; i < networkBytes.Length; i++)
             {
-                broadcastBytes[i] = (byte)(networkBytes[i] | (maskBytes[i] ^ 255));
+                broadcastBytes[i] = (byte)(networkBytes[i] | ~maskBytes[i]);
             }
             return new IPAddress(broadcastBytes);
         }
 
-        static IPAddress GetFirstIPAddress(IPAddress networkAddress)
+        private IPAddress GetFirstIPAddress(IPAddress networkAddress)
         {
             byte[] ipBytes = networkAddress.GetAddressBytes();
             ipBytes[ipBytes.Length - 1] += 1;
             return new IPAddress(ipBytes);
         }
 
-        static IPAddress GetLastIPAddress(IPAddress broadcastAddress)
+        private IPAddress GetLastIPAddress(IPAddress broadcastAddress)
         {
             byte[] ipBytes = broadcastAddress.GetAddressBytes();
             ipBytes[ipBytes.Length - 1] -= 1;
             return new IPAddress(ipBytes);
         }
 
-        static int GetAvailableIPCount(IPAddress subnetMask)
+        private int GetAvailableIPCount(IPAddress subnetMask)
         {
             byte[] maskBytes = subnetMask.GetAddressBytes();
             uint mask = BitConverter.ToUInt32(maskBytes.Reverse().ToArray(), 0);
             return (int)(~mask - 1);
+        }
+
+        private int GetAvailableMachinesCount(IPAddress subnetMask)
+        {
+            return GetAvailableIPCount(subnetMask) - 2;
         }
     }
 }
